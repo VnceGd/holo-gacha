58d16ac5c7ddb1ee01bfda67d0cdf6aa6b63c6e0
Vincent Nguyen <vtn28@drexel.edu>
2020-08-30 11:43:30 -0400

    Test addition of 'holo-gacha' to relative path
    * Also shorten service worker code with arrow functions

--- service-worker.js
+++ service-worker.js
@@ -7,56 +7,54 @@ let urlsToCache = ['/']
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
     // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then(function(cache) {
+        .then( cache => {
             return cache.addAll(urlsToCache)
         })
     )
 })
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-        .then(function(response) {
+        .then( response => {
             // Cache hit - return response
             if (response) {
                 return response
             }
             return fetch(event.request)
-            .then(
-                function(response) {
-                    // Check if we received a valid response
-                    if (!response || response.status !== 200 || response.type !== 'basic') {
-                        return response
-                    }
-
-                    // IMPORTANT: Clone the response. A response is a stream
-                    // and because we want the browser to consume the response
-                    // as well as the cache consuming the response, we need
-                    // to clone it so we have two streams.
-                    let responseToCache = response.clone()
-        
-                    caches.open(CACHE_NAME)
-                    .then(function(cache) {
-                        cache.put(event.request, responseToCache)
-                    })
+            .then( response => {
+                // Check if we received a valid response
+                if (!response || response.status !== 200 || response.type !== 'basic') {
                     return response
                 }
-            )
+
+                // IMPORTANT: Clone the response. A response is a stream
+                // and because we want the browser to consume the response
+                // as well as the cache consuming the response, we need
+                // to clone it so we have two streams.
+                let responseToCache = response.clone()
+    
+                caches.open(CACHE_NAME)
+                .then( cache => {
+                    cache.put(event.request, responseToCache)
+                })
+                return response
+            })
         })
     )
 })
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', event => {
     let cacheAllowlist = ['holo-cache']
 
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
+        caches.keys().then( cacheNames => {
             return Promise.all(
-                cacheNames.map(function(cacheName) {
+                cacheNames.map( cacheName => {
                     if (cacheAllowlist.indexOf(cacheName) === -1) {
                         return caches.delete(cacheName)
                     }
