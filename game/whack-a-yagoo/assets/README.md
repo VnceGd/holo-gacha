361d42b6b7dbf21ce3d81350e9715e11689f9f8d
Vincent Nguyen <vtn28@drexel.edu>
2020-09-06 13:20:38 -0400

    Add Whack-a-YAGOO minigame

---
+++ game/whack-a-yagoo/assets/README.md
@@ -0,0 +1,7 @@
+# Whack-a-YAGOO Resources
+
+[`img/yagoo.jpg`](img/yagoo.jpg)
+
+[`img/yagoo.png`](img/yagoo.png) - *Edited in GIMP-2.10*
+
+https://pbs.twimg.com/profile_images/1186979284319006720/gH6xdlYB_400x400.jpg
---
+++ game/whack-a-yagoo/assets/js/index.js
@@ -0,0 +1,156 @@
+let maxRows = 3
+let maxCols = 3
+let maxSpawns = 50
+let spawnInterval = 1000 // ms
+let stayDuration = 3000 // ms
+let hitCount = 0
+
+let positions = []
+let timeouts = []
+
+// Animate image sliding in from bottom
+function animateYagoo(image, reverse) {
+    return new Promise( resolve => {
+        image.animate([
+            // keyframes
+            { transform: 'translateY(18vmin)' },
+            { transform: 'translateY(0)' }
+        ], {
+            // timing options
+            duration: 200,
+            easing: 'ease-out',
+            direction: reverse
+        })
+        setTimeout( _ => {
+            resolve('yagoo animated')
+        }, 150)
+    })
+}
+
+// Create image element at random unoccupied tile
+function spawnYagoo() {
+    let positionX = Math.floor(Math.random() * maxCols)
+    let positionY = Math.floor(Math.random() * maxRows)
+    let position = [positionX, positionY]
+    let attempt = 0
+
+    while (positions.find(e => e[0] === positionX && e[1] === positionY)) {
+        positionX = Math.floor(Math.random() * maxCols)
+        positionY = Math.floor(Math.random() * maxRows)
+        position = [positionX, positionY]
+
+        if (attempt > 10) break
+
+        attempt++
+    }
+
+    positions.push(position)
+
+    let yagooImage = document.createElement('img')
+
+    yagooImage.src = 'assets/img/yagoo.png'
+    document.getElementById(`${positionX},${positionY}`).appendChild(yagooImage)
+    animateYagoo(yagooImage, 'normal')
+
+    let timeout =
+    setTimeout( _ => {
+        despawnYagoo(position)
+    }, stayDuration)
+
+    timeouts.push(timeout)
+}
+
+// Remove child of tile at position
+async function despawnYagoo(position, whack) {
+    let positionX = position[0]
+    let positionY = position[1]
+    let tile = document.getElementById(`${positionX},${positionY}`)
+
+    if (!tile.hasChildNodes) return
+
+    let positionIndex = positions.indexOf(position)
+
+    if (positionIndex < 0) {
+        positions.find( e => {
+            e[0] === positionX && e[1] === positionY
+            positionIndex = positions.indexOf(e)
+        })
+    }
+
+    if (positionIndex > -1) {
+        let yagooImage = tile.childNodes[0]
+
+        if (yagooImage === undefined) return
+
+        if (whack) {
+            yagooImage.style.height = '9vmin'
+            hitCount++
+        }
+
+        await animateYagoo(yagooImage, 'reverse')
+
+        tile.removeChild(yagooImage)
+        positions.splice(positionIndex, 1)
+        timeouts.splice(positionIndex, 1)
+    }
+}
+
+// Squash child of tile at position then despawn
+function whackYagoo(position) {
+    despawnYagoo(position, whack = true)
+}
+
+// Create grid of size maxCols x maxRows
+function generateLayout() {
+    let game = document.getElementById('game')
+
+    for (let r = 0; r < maxRows; r++) {
+        let rowDiv = document.createElement('div')
+
+        rowDiv.className = 'row'
+
+        for (let c = 0; c < maxCols; c++) {
+            let tile = document.createElement('div')
+
+            tile.className = 'tile'
+            tile.id = `${c},${r}`
+            tile.onclick = _ => { whackYagoo([c, r]) }
+            rowDiv.appendChild(tile)
+        }
+
+        game.appendChild(rowDiv)
+    }
+}
+
+// Generate grid then start spawning YAGOOs
+function startGame() {
+    hitCount = 0
+
+    generateLayout()
+
+    for (let s = 0; s < maxSpawns; s++) {
+        setTimeout(spawnYagoo, spawnInterval * s)
+    }
+
+    setTimeout(endGame, spawnInterval * maxSpawns)
+}
+
+// Show game end panel
+function endGame() {
+    document.getElementById('hit-count').innerHTML = hitCount
+    document.getElementById('game-over-panel').style.display = 'block'
+}
+
+// Close game end panel
+function closeEndGamePanel() {
+    document.getElementById('game-over-panel').style.display = 'none'
+}
+
+// Close game end panel and start new game
+function restartGame() {
+    closeEndGamePanel()
+    startGame()
+}
+
+// Start game on load
+window.onload = _ => { startGame() }
