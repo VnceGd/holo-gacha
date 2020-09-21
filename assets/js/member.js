b1da7061bcc90709b3a3c169e1a1c5b93a3f395f
Vincent Nguyen <vtn28@drexel.edu>
2020-09-21 19:52:24 -0400

    Add member sorting
    * Set default sorting to generation
    * Move button generation to separate function
    * Name sort uses values in member object (first names)

--- assets/js/member.js
+++ assets/js/member.js
@@ -1,16 +1,21 @@
 const member = {
     AQUA: 'aqua',
-    GURA: 'gura',
     PEKORA: 'pekora',
+    WATAME: 'watame',
     POLKA: 'polka',
-    WATAME: 'watame'
+    GURA: 'gura'
 }
 const area = {
     HEAD: 'tap-head',
     CHEST: 'tap-chest'
 }
+const sort = {
+    GEN: 'gen',
+    NAME: 'name'
+}
 
 let currentMember = member.POLKA
+let currentSort = sort.GEN
 let isMemberPopulated = false
 
 // Update and cache member
@@ -26,13 +31,41 @@ function setMember(_member) {
 function populateMemberGrid() {
     if (isMemberPopulated) return
 
+    generateButtons(Object.values(member))
+    isMemberPopulated = true
+}
+
+// Sort member grid buttons based on select element value
+function sortMemberGrid() {
+    let memberArray = []
+    let sortingMethod = document.getElementById('member-sort-select').value
+
     Object.values(member).forEach(m => {
+        memberArray.push(m)
+    })
+
+    switch (sortingMethod) {
+        case sort.NAME:
+            if (currentSort == sort.NAME) return
+            memberArray = memberArray.sort()
+            break
+        case sort.GEN:
+            if (currentSort == sort.GEN) return
+            break
+    }
+
+    document.getElementById('member-grid').innerHTML = ''
+    generateButtons(memberArray)
+    currentSort = sortingMethod
+}
+
+// Generate buttons for each element in the list
+function generateButtons(_list) {
+    _list.forEach(m => {
         let templateButton = document.createElement('button')
 
         templateButton.onclick = () => { setMember(m) }
         templateButton.innerHTML = `<div class="background"></div><img loading="lazy" src="assets/img/${m}/full.png">`
         document.getElementById('member-grid').appendChild(templateButton)
     })
-
-    isMemberPopulated = true
 }
