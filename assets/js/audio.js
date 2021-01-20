7504639c614fe3666daffb968ce5ab4036438846
Vincent Nguyen <vtn28@drexel.edu>
2021-01-20 16:30:17 -0500

    Separate controls for voice and sfx volume

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,3 +1,8 @@
+const audio = {
+  Voice: 'voice',
+  SFX: 'sfx',
+}
+
 const MAX_VOLUME = 100
 const MIN_VOLUME = 0
 
@@ -6,16 +11,23 @@ let currentAudio = new Audio()
 let currentSFX = new Audio()
 
 // Update and cache volume
-function setVolume(_volume) {
-  currentAudio.volume = _volume / 100
-  currentSFX.volume = _volume / 100
-  localStorage.setItem('volume', _volume)
+function setVolume(_type, _volume) {
+  switch (_type) {
+    case audio.Voice:
+      currentAudio.volume = _volume / 100
+      break
+    case audio.SFX:
+      currentSFX.volume = _volume / 100
+      break
+  }
+
+  localStorage.setItem(`${_type}-volume`, _volume)
 }
 
 // Set currentAudio volume level and update paired input value
-function changeVolume(_value = false) {
-  let inputRange = document.getElementById('audio')
-  let inputNumber = document.getElementById('audio-value')
+function changeVolume(_type, _value = false) {
+  let inputRange = document.getElementById(`${_type}`)
+  let inputNumber = document.getElementById(`${_type}-value`)
 
   if (_value) {
     let inputVolume = inputNumber.value
@@ -40,7 +52,7 @@ function changeVolume(_value = false) {
     inputNumber.value = inputRange.value
   }
 
-  setVolume(inputRange.value)
+  setVolume(_type, inputRange.value)
 }
 
 // Call changeVolume with _value = true to update paired input range
@@ -50,17 +62,13 @@ function changeVolumeValue() {
 
 // Play sound effect given filename
 function playSoundEffect(_effect) {
-  let sfx = `assets/audio/sfx/${_effect}.mp3`
-
-  currentSFX.src = sfx
+  currentSFX.src = `assets/audio/sfx/${_effect}.mp3`
   currentSFX.play()
 }
 
 // Set audio source and play audio
 function playAudioClip(_member, _clip) {
-  let clip = `assets/audio/${_member['FILE']}/${_clip}.mp3`
-
-  currentAudio.src = clip
+  currentAudio.src = `assets/audio/${_member['FILE']}/${_clip}.mp3`
   currentAudio.play()
 }
 
@@ -69,14 +77,30 @@ function stopAudio() {
   if (!currentAudio.paused) currentAudio.pause()
 }
 
+// Load inputs for changing audio in settings menu
+function loadAudioInput() {
+  let inputDiv = document.getElementById('audio-input')
+  for (const key in audio) {
+    let value = audio[key]
+    let template = `
+    <p>${key}</p>
+    <input class="audio-range" id="${value}" type="range" list="volume-levels" value="100" onchange="changeVolume('${value}')" oninput="changeVolume('${value}')">
+    <input class="audio-value" id="${value}-value" type="number" value="100" min="0" max="100" onchange="changeVolumeValue('${value}')"></input>`
+    inputDiv.innerHTML += template
+  }
+}
+
 // Load volume value from cache
 function loadVolumeSettings() {
-  let cachedVolume = localStorage.getItem('volume')
+  for (const key in audio) {
+    let value = audio[key]
+    let cachedVolume = localStorage.getItem(`${value}-volume`)
 
-  if (cachedVolume != null) {
-    setVolume(cachedVolume)
-    document.getElementById('audio').value = cachedVolume
-    document.getElementById('audio-value').value = cachedVolume
+    if (cachedVolume != null) {
+      setVolume(value, cachedVolume)
+      document.getElementById(`${value}`).value = cachedVolume
+      document.getElementById(`${value}-value`).value = cachedVolume
+    }
   }
 }
 
@@ -93,6 +117,7 @@ function loadAudio() {
     })
   }
 
+  loadAudioInput()
   loadVolumeSettings()
 }
 
